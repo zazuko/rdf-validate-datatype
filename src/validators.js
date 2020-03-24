bd879879dd8b0efcabb6eebed1148d2a0637c230
Martin Maillard <self@martin-maillard.com>
2020-03-24 11:34:35 +0100

    feat: add simple validators for number/date types

--- src/validators.js
+++ src/validators.js
@@ -42,4 +42,94 @@ const validators = new Registry()
 
 validators.register(xsd.string, value => true)
 
+validators.register(xsd.integer, value => {
+  const parsed = parseInt(value)
+  return (
+    /^(\+|-)?\d+$/.test(value) &&
+    !isNaN(parsed) &&
+    parsed >= -2147483648 &&
+    parsed <= 2147483647
+  )
+})
+
+validators.register(xsd.boolean, value => (
+  value === '1' ||
+  value === 'true' ||
+  value === '0' ||
+  value === 'false'
+))
+
+validators.register(xsd.decimal, value => /^(\+|-)?\d+(\.\d+)?$/.test(value))
+
+validators.register(xsd.float, validateFloat)
+validators.register(xsd.double, validateFloat)
+
+function validateFloat (value) {
+  return (
+    value === 'INF' ||
+    value === '-INF' ||
+    value === 'NaN' ||
+    /^(\+|-)?\d+(\.\d+)?((E|e)(\+|-)?\d+)?$/.test(value)
+  )
+}
+
+const dateSignSeg = '-?'
+const durationYearsSeg = '\\d+Y'
+const durationMonthsSeg = '\\d+M'
+const durationDaysSeg = '\\d+D'
+const durationHoursSeg = '\\d+H'
+const durationMinutesSeg = '\\d+M'
+const durationSecondsSeg = '\\d+(\\.\\d+)?S'
+// All the segments are optional, but at least one of them must be specified.
+// TODO: Is there a cleaner way to specify this?
+const durationTimeSeg = `(T(${durationHoursSeg})(${durationMinutesSeg})?(${durationSecondsSeg})?)|((${durationHoursSeg})?(${durationMinutesSeg})(${durationSecondsSeg})?)|((${durationHoursSeg})?(${durationMinutesSeg})?(${durationSecondsSeg}))`
+const durationSeg = `${dateSignSeg}P(${durationYearsSeg})?(${durationMonthsSeg})?(${durationDaysSeg})?(${durationTimeSeg})?`
+
+const durationPattern = new RegExp(`^${durationSeg}$`)
+validators.register(xsd.duration, value => durationPattern.test(value))
+
+const yearSeg = `${dateSignSeg}\\d{4}`
+const timezoneSeg = '(((\\+|-)\\d{2}:\\d{2})|Z)?'
+const monthSeg = '\\d{2}'
+const daySeg = '\\d{2}'
+const dateSeg = `${yearSeg}-${monthSeg}-${daySeg}`
+const timeSeg = '\\d{2}:\\d{2}:\\d{2}(\\.\\d+)?'
+const dateTimeSeg = `${dateSeg}T${timeSeg}${timezoneSeg}`
+
+const dateTimePattern = new RegExp(`^${dateTimeSeg}$`)
+validators.register(xsd.dateTime, value => dateTimePattern.test(value))
+
+const datePattern = new RegExp(`^${dateSeg}${timezoneSeg}$`)
+validators.register(xsd.date, value => datePattern.test(value))
+
+const dayPattern = new RegExp(`^${daySeg}${timezoneSeg}$`)
+validators.register(xsd.gDay, value => dayPattern.test(value))
+
+const monthPattern = new RegExp(`^${monthSeg}${timezoneSeg}$`)
+validators.register(xsd.gMonth, value => monthPattern.test(value))
+
+const monthDayPattern = new RegExp(`^${monthSeg}-${daySeg}${timezoneSeg}$`)
+validators.register(xsd.gMonthDay, value => monthDayPattern.test(value))
+
+const yearPattern = new RegExp(`^${yearSeg}${timezoneSeg}$`)
+validators.register(xsd.gYear, value => yearPattern.test(value))
+
+const yearMonthPattern = new RegExp(`^${yearSeg}-${monthSeg}${timezoneSeg}$`)
+validators.register(xsd.gYearMonth, value => yearMonthPattern.test(value))
+
+const timePattern = new RegExp(`^${timeSeg}${timezoneSeg}$`)
+validators.register(xsd.time, value => timePattern.test(value))
+
+// TODO
+validators.register(xsd.hexBinary, value => true)
+
+// TODO
+validators.register(xsd.base64Binary, value => true)
+
+// TODO
+validators.register(xsd.anyURI, value => true)
+
+// TODO
+validators.register(xsd.QName, value => true)
+
 module.exports = validators
--- test/test-validate-term.js
+++ test/test-validate-term.js
@@ -1,6 +1,7 @@
 /* eslint-env mocha */
 const assert = require('assert')
 const $rdf = require('@rdfjs/data-model')
+const { xsd } = require('@tpluscode/rdf-ns-builders')
 
 const { validateTerm } = require('../index')
 
@@ -19,5 +20,137 @@ describe('#validateTerm', () => {
   it('throws on blank nodes', () => {
     const term = $rdf.blankNode()
     assert.throws(() => validateTerm(term), Error)
+  })
+
+  ;[
+    ['test', xsd.string, true],
+
+    ['1', xsd.integer, true],
+    ['test', xsd.integer, false],
+    ['1231343123123', xsd.integer, false, 'too large'],
+    ['123x', xsd.integer, false, 'trailing string'],
+
+    ['true', xsd.boolean, true],
+    ['false', xsd.boolean, true],
+    ['test', xsd.boolean, false],
+
+    ['0.4', xsd.decimal, true],
+    ['244.14', xsd.decimal, true],
+    ['244', xsd.decimal, true, 'without decimal digits'],
+    ['-24.4', xsd.decimal, true, 'minus sign'],
+    ['+24.4', xsd.decimal, true, 'plus sign'],
+    ['test', xsd.decimal, false],
+
+    ['-1E4', xsd.float, true],
+    ['1267.43233E12', xsd.float, true],
+    ['12.78e-2', xsd.float, true],
+    ['12', xsd.float, true],
+    ['-0', xsd.float, true],
+    ['0', xsd.float, true],
+    ['INF', xsd.float, true],
+    ['-INF', xsd.float, true],
+    ['NaN', xsd.float, true],
+    ['test', xsd.float, false],
+    ['10e4.2', xsd.float, false],
+
+    ['-1E4', xsd.double, true],
+    ['1267.43233E12', xsd.double, true],
+    ['12.78e-2', xsd.double, true],
+    ['12', xsd.double, true],
+    ['-0', xsd.double, true],
+    ['0', xsd.double, true],
+    ['INF', xsd.double, true],
+    ['-INF', xsd.double, true],
+    ['NaN', xsd.double, true],
+    ['test', xsd.double, false],
+    ['10e4.2', xsd.double, false],
+
+    ['P1Y2M3DT10H30M', xsd.duration, true],
+    ['-P120D', xsd.duration, true],
+    ['P1347Y', xsd.duration, true],
+    ['P1347M', xsd.duration, true],
+    ['P1Y2MT2H', xsd.duration, true],
+    ['P0Y1347M', xsd.duration, true],
+    ['P0Y1347M0D', xsd.duration, true],
+    ['-P1347M', xsd.duration, true],
+    ['P-1347M', xsd.duration, false],
+    ['P1Y2MT', xsd.duration, false],
+    ['test', xsd.duration, false],
+
+    ['2020-04-02', xsd.date, true],
+    ['-2020-04-02', xsd.date, true],
+    ['2020-04-02Z', xsd.date, true],
+    ['2020-04-02+03:00', xsd.date, true],
+    ['2020-04-02x', xsd.date, false],
+    ['test', xsd.date, false],
+
+    ['2020-04-02T23:33:12.234Z', xsd.dateTime, true],
+    ['2020-04-02T23:33:12.234+04:00', xsd.dateTime, true],
+    ['2020-04-02T23:33:12+04:00', xsd.dateTime, true],
+    ['2020-04-02T23:33:12', xsd.dateTime, true],
+    ['2020-04-02T23:33:12x', xsd.dateTime, false],
+    ['2020-04-02T23:33', xsd.dateTime, false],
+    ['test', xsd.dateTime, false],
+
+    ['23:33:12.234', xsd.time, true],
+    ['03:43:12.234Z', xsd.time, true],
+    ['23:33:12.134+04:00', xsd.time, true],
+    ['17:33:12.23-01:00', xsd.time, true],
+    ['13:20:00-05:00', xsd.time, true],
+    ['23:33:12', xsd.time, true],
+    ['23:33:12x', xsd.time, false],
+    ['23:33', xsd.time, false],
+    ['test', xsd.time, false],
+
+    ['12', xsd.gDay, true],
+    ['30Z', xsd.gDay, true],
+    ['01+01:00', xsd.gDay, true],
+    ['04-12:00', xsd.gDay, true],
+    ['1', xsd.gDay, false],
+    ['-13', xsd.gDay, false],
+    ['test', xsd.gDay, false],
+
+    ['12', xsd.gMonth, true],
+    ['03Z', xsd.gMonth, true],
+    ['01+01:00', xsd.gMonth, true],
+    ['04-12:00', xsd.gMonth, true],
+    ['1', xsd.gMonth, false],
+    ['-12', xsd.gMonth, false],
+    ['test', xsd.gMonth, false],
+
+    ['04-02', xsd.gMonthDay, true],
+    ['12-22', xsd.gMonthDay, true],
+    ['12-22Z', xsd.gMonthDay, true],
+    ['12-22+04:00', xsd.gMonthDay, true],
+    ['12-22-04:00', xsd.gMonthDay, true],
+    ['2020-12-22', xsd.gMonthDay, false],
+    ['-12-22', xsd.gMonthDay, false],
+    ['12-22X', xsd.gMonthDay, false],
+    ['test', xsd.gMonthDay, false],
+
+    ['2020', xsd.gYear, true],
+    ['-2020', xsd.gYear, true],
+    ['2020Z', xsd.gYear, true],
+    ['2020+04:00', xsd.gYear, true],
+    ['-2020-02:00', xsd.gYear, true],
+    ['2020-12-01', xsd.gYear, false],
+    ['test', xsd.gYear, false],
+
+    ['2020-12', xsd.gYearMonth, true],
+    ['-2020-04', xsd.gYearMonth, true],
+    ['2020-04Z', xsd.gYearMonth, true],
+    ['2020-11+04:00', xsd.gYearMonth, true],
+    ['-2020-05-02:00', xsd.gYearMonth, true],
+    ['2020-12-01', xsd.gYearMonth, false],
+    ['2020-04X', xsd.gYearMonth, false],
+    ['test', xsd.gYearMonth, false]
+  ].forEach(([input, datatype, expected, remark]) => {
+    const datatypeName = datatype.value.split('#').slice(-1)[0]
+    const titleRemark = remark ? ` (${remark})` : ''
+    it(`validates ${expected ? 'valid' : 'invalid'} ${datatypeName}${titleRemark}: "${input}"`, () => {
+      const term = $rdf.literal(input, datatype)
+      const isValid = validateTerm(term)
+      assert.strictEqual(isValid, expected)
+    })
   })
 })
